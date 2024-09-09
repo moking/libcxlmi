3ca3a04283710c6ce182774ed1dce515110c0055
Fan Ni <fan.ni@samsung.com>
2024-09-09 10:03:43 -0700

    dcd: Add command to get DC region configuration
    With the change, we add mailbox command to "Retrieve Dynamic the
    DC Capacity Region configuration of the (Opcode device. 4800h)", pre
    CXL spec r3.1 section 8.2.9.9.9.1.
    
    Signed-off-by: Fan Ni <fan.ni@samsung.com>

--- src/libcxlmi.h
+++ src/libcxlmi.h
@@ -555,7 +555,10 @@ int cxlmi_cmd_fmapi_set_qos_bw_limit(struct cxlmi_endpoint *ep,
 			struct cxlmi_tunnel_info *ti,
 			struct cxlmi_cmd_fmapi_set_qos_bw_limit *in,
 			struct cxlmi_cmd_fmapi_set_qos_bw_limit *ret);
-
+int cxlmi_cmd_memdev_get_dc_config(struct cxlmi_endpoint *ep,
+			struct cxlmi_tunnel_info *ti,
+			struct cxlmi_cmd_memdev_get_dc_config_req *in,
+			struct cxlmi_cmd_memdev_get_dc_config_rsp *ret);
 #ifdef __cplusplus
 }
 #endif
